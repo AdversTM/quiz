93b7307501e1cf85bdb5b06468e8e00be678bf79
Damian Szuster <adverstm@gmail.com>
2022-04-07 23:55:23 +0200

    Update

--- quiz/model/Answer.cs
+++ quiz/model/Answer.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace quiz.model {
+﻿namespace quiz.model {
     public class Answer {
         public string Text { get; set; }
         public bool IsCorrect { get; set; }
@@ -16,10 +14,6 @@ namespace quiz.model {
         public override bool Equals(object obj) {
             if (obj is not Answer o) return false;
             return Text == o.Text && IsCorrect == o.IsCorrect;
-        }
-
-        public object Clone() {
-            return new Answer(Text, IsCorrect);
         }
 
         public override string ToString() {
